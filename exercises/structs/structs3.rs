496ef7639a9b28611c54b84cd68357a8b34b3ddf
root <root@LAPTOP-TSVCRT6E.localdomain>
2023-10-26 10:28:26 +0800

    modify by mike meng

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,15 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.sender_country.eq(&self.recipient_country) {
+            return false;
+        }
+        return true;
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        return self.weight_in_grams * cents_per_gram;
     }
 }
 
