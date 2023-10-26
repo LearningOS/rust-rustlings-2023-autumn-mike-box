5577ed7f9002ef8fc2cfca4f38eb8b61f4a9804b
root <root@LAPTOP-TSVCRT6E.localdomain>
2023-10-26 08:58:30 +0800

    modify by mike meng

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -5,21 +5,19 @@
 
 // I AM NOT DONE
 
-fn trim_me(input: & str) -> String {
+fn trim_me(input: str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    String::new("123");
+    return str.trim().to_string();
 }
 
 fn compose_me(input: & str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    str.push_str(" world!");
-    str;
+    return str.push_str(" world!").to_string();
 }
 
 fn replace_me(input: & str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    str.replace("cars", "balloons");
-    str;
+    return str.replace("cars", "balloons").to_string();
 }
 
 #[cfg(test)]
