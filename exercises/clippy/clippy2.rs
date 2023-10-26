9a0fd8f387affc866b4d4586358a1320bdd89fac
root <root@LAPTOP-TSVCRT6E.localdomain>
2023-10-26 12:24:52 +0800

    modify by mike meng

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -7,8 +7,8 @@
 
 fn main() {
     let mut res = 42;
-    let option = Some(12);
-    for x in option {
+    let option : Option<i32> = Some(12);
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
