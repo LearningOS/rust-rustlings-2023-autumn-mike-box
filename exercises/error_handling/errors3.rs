c610165e8ec024dafef5124ddef58dc23aa701e5
root <root@LAPTOP-TSVCRT6E.localdomain>
2023-10-27 10:28:06 +0800

    modify by mike meng

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -16,7 +16,7 @@ fn main() {
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
-
+    Ok(cost);
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
@@ -29,6 +29,8 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>()?;
-
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Ok(x) => Ok(x * cost_per_item + processing_fee),
+        Err(e) => Err(e),
+    }
 }
